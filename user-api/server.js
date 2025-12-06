16c0f349b0324e1b14e731503690bf1523b726e3
Yigit Dalkilic <ygtdalkilic@gmail.com>
2025-12-05 22:33:25 -0500

    WEB422 Assignment 3 - Next.js app with authentication

--- user-api/server.js
+++ user-api/server.js
@@ -4,11 +4,42 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const userService = require("./user-service.js");
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const jwt = require("jsonwebtoken");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("JWT"),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log("payload received", jwt_payload);
+
+  if (jwt_payload) {
+    next(null, { 
+      _id: jwt_payload._id, 
+      userName: jwt_payload.userName 
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
+// Initialize passport
+app.use(passport.initialize());
 
 app.post("/api/user/register", (req, res) => {
     userService.registerUser(req.body)
@@ -22,39 +53,57 @@ app.post("/api/user/register", (req, res) => {
 app.post("/api/user/login", (req, res) => {
     userService.checkUser(req.body)
     .then((user) => {
-        res.json({ "message": "login successful"});
+        // Create payload with user info
+        let payload = {
+          _id: user._id,
+          userName: user.userName
+        };
+
+        // Sign the payload to create token
+        let token = jwt.sign(payload, process.env.JWT_SECRET);
+
+        // Send token back to client
+        res.json({ message: "login successful", token: token });
     }).catch(msg => {
         res.status(422).json({ "message": msg });
     });
 });
 
-app.get("/api/user/favourites", (req, res) => {
-    userService.getFavourites(req.user._id)
-    .then(data => {
-        res.json(data);
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
-    })
+app.get("/api/user/favourites", 
+    passport.authenticate("jwt", { session: false }), 
+    (req, res) => {
+        userService.getFavourites(req.user._id)
+        .then(data => {
+            res.json(data);
+        }).catch(msg => {
+            res.status(422).json({ error: msg });
+        })
+    }
+);
 
-});
+app.put("/api/user/favourites/:id", 
+    passport.authenticate("jwt", { session: false }), 
+    (req, res) => {
+        userService.addFavourite(req.user._id, req.params.id)
+        .then(data => {
+            res.json(data)
+        }).catch(msg => {
+            res.status(422).json({ error: msg });
+        })
+    }
+);
 
-app.put("/api/user/favourites/:id", (req, res) => {
-    userService.addFavourite(req.user._id, req.params.id)
-    .then(data => {
-        res.json(data)
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
-    })
-});
-
-app.delete("/api/user/favourites/:id", (req, res) => {
-    userService.removeFavourite(req.user._id, req.params.id)
-    .then(data => {
-        res.json(data)
-    }).catch(msg => {
-        res.status(422).json({ error: msg });
-    })
-});
+app.delete("/api/user/favourites/:id", 
+    passport.authenticate("jwt", { session: false }), 
+    (req, res) => {
+        userService.removeFavourite(req.user._id, req.params.id)
+        .then(data => {
+            res.json(data)
+        }).catch(msg => {
+            res.status(422).json({ error: msg });
+        })
+    }
+);
 
 userService.connect()
 .then(() => {
